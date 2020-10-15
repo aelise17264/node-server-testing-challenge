b0c1734ab45c10f10a24464dc31694f7157a507b
unknown <aelise17264@gmail.com>
2020-10-15 15:42:57 -0600

    all tests passing

--- data/characters.test.js
+++ data/characters.test.js
@@ -1,5 +1,7 @@
 const { create, remove, getCharacters} = require('./characters/model')
 
+const db = require('./dbConfig')
+
 describe('getCharacters', () => {
     it('should return the list of characters', () => {
         const characterList = getCharacters()
@@ -7,3 +9,18 @@ describe('getCharacters', () => {
     })
 })
 
+describe('create new character', () => {
+    it('should add a character to the list', async () => {
+       await create ({name: 'Cesar'})
+       const characters = await db('characters')
+        expect(characters).toHaveLength(1)
+    })
+})
+
+describe('remove a character', () => {
+    it('should remove a character from the list', async () => {
+        await remove ({name: 'Cesar'})
+        const characters = await db('characters')
+        expect(characters).toHaveLength(0)
+    })
+})
